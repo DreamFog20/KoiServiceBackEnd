17d425e66a5b0b9945ccce9a6947a9ff9ff75c34
DreamFog20 <thaidat011003@gmail.com>
2024-10-21 14:38:16 +0700

    Booking

--- src/main/java/com/example/profile_api/repository/PaymentRepository.java
+++ src/main/java/com/example/profile_api/repository/PaymentRepository.java
@@ -3,11 +3,13 @@ package com.example.profile_api.repository;
 
 import com.example.profile_api.model.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
 
-        boolean existsByVnp_TransactionNo (String vnp_TransactionNo);
+        @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END FROM Payment p WHERE p.vnp_TransactionNo = :vnp_TransactionNo")
+        boolean existsByVnp_TransactionNo(String vnp_TransactionNo);
 
 }
