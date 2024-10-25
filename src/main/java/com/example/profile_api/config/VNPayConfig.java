79b39b992e6b771cbda54d434cbb1307ac069733
DreamFog20 <thaidat011003@gmail.com>
2024-10-25 13:38:20 +0700

    Update VNPayConfig

--- src/main/java/com/example/profile_api/config/VNPayConfig.java
+++ src/main/java/com/example/profile_api/config/VNPayConfig.java
@@ -21,7 +21,7 @@ import java.util.*;
 
 public class VNPayConfig {
     public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-    public static String vnp_ReturnUrl = "http://localhost:5173/Booking";
+    public static String vnp_ReturnUrl = "http://localhost:5173/PaymentResult";
     public static String vnp_TmnCode = "VVHLKKC6";
     public static String vnp_Version = "2.1.0";
     public static String vnp_Command = "pay";
